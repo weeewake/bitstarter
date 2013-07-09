29165b22ccfd809b4997e15b1f79217c8c45446c
Vivek Sundaram <weeewake@gmail.com>
2013-07-09 23:14:59 +0000

    Read index and write to response

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  // read index.html
+  fileContents = fs.readFileSync('./index.html');
+
+  if (fileContents) {
+  	response.send(fileContents);	
+  }
+   
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
